1241b202f26bf31086f2f155a1bceaac0797a624
ilyata76 <ilyata76@yandex.ru>
2021-04-25 20:11:36 +0300

    Improve CALEN classes

--- src/calen/calen.hpp
+++ src/calen/calen.hpp
@@ -3,12 +3,13 @@
 #ifndef CALEN_HPP
 #define CALEN_HPP
 
-	#include "../dies/date.hpp"
+	//#include "../dies/date.hpp"
 	#include "./calenmonth.hpp"
 	#include <iostream>
 
 	namespace calen {
-
+		// TODO B: сделать разбиение всего календаря на годы, годы на месяцы, месяцы на дни, чтобы обращаться можно было к каждому из них
+			// как метод или как надстройку?
 		class calen {
 		private:
 			int orientation;
@@ -22,15 +23,26 @@
 			calenday startingDay;
 			calenday endingDay;
 
+			// B
+			int years;
+			std::vector <calenmonth> arrayOfMonths;
+			// !B
+
 		public:
 			calen() { 
 				this->orientation = 0; this->startingYear = 1; this->endingYear = 1; 
-				this->startingMonth; this->endingMonth; this->startingDay; this->endingDay;
+				//this->startingMonth; this->endingMonth; this->startingDay; this->endingDay;
+				this->years = 0;
 			};
-			calen(int ORIENT, int stYear, int enYear, calenmonth stMonth, calenmonth enMonth, calenday stDay, calenday enDay) {
+			calen(int ORIENT, int stYear, int enYear, calenmonth stMonth, calenmonth enMonth, calenday stDay, calenday enDay, std::vector <calenmonth> aOM) {
 				this->orientation = ORIENT; this->startingYear = stYear; this->endingYear = enYear;
 				this->startingMonth = stMonth; this->endingMonth = enMonth; this->startingDay = stDay; this->endingDay = enDay;
+				this->arrayOfMonths = aOM;
 			};
+
+			void calenOC(calen object); // out-console
+			void calenOF(calen object); // out-file
+
 			~calen() {};
 		};
 
--- src/calen/calenmonth.hpp
+++ src/calen/calenmonth.hpp
@@ -6,6 +6,8 @@
 
 	#include "./calenday.hpp"
 
+	#include <vector>
+
 	namespace calen {
 
 		class calenmonth {
@@ -14,10 +16,15 @@
 			int days;
 			calenday startingDay;
 			calenday endingDay;
+
+			// B
+			std::vector <calenday> arrayOfDays;
+			// !B
+
 		public:
 			calenmonth() { this->weaks = 0; this->days = 0; this->startingDay = { 1,1,1 }; this->endingDay = { 1,1,1 }; };
-			calenmonth(int Weaks, int Days, calenday stDay, calenday enDay) { 
-				this->weaks = Weaks; this->days = Days; this->startingDay = stDay; this->endingDay = enDay;
+			calenmonth(int Weaks, int Days, calenday stDay, calenday enDay, std::vector <calenday> aOD) {
+				this->weaks = Weaks; this->days = Days; this->startingDay = stDay; this->endingDay = enDay; this->arrayOfDays = aOD;
 			};
 			~calenmonth() {};
 		};
